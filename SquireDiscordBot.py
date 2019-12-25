f1516e5789a1cd4015f54c7ba681db457aa54f25
Knight-Law <xlawdizx@gmail.com>
2019-12-24 17:24:26 -0800

    Added Events on Messages based on UserID

--- SquireDiscordBot.py
+++ SquireDiscordBot.py
@@ -6,26 +6,74 @@ import requests
 from discord import Game
 from discord.ext.commands import Bot
 
+try:
+    fp = open('C:/Users/Lawrence/Desktop/token.txt','r')
+    TOKEN = fp.read()
+finally:
+    fp.close()
+
 BOT_PREFIX = (".")
-STEAMAPI = '?'
-TOKEN = '?'
 
 client = Bot(command_prefix=BOT_PREFIX)
 
-@client.command(name='whosucks',
-                description="TBD",
-                brief="TBD",
-                aliases=['TBD'],
-                pass_context=True)
-async def suckList(context):
-    possible_responses = [
-        'Mark Sucks',
-        'Hy Sucks',
-        'Michael Sucks',
-        'Jen Sucks',
-        'Everyone is awesome',
-    ]
-    await client.say(random.choice(possible_responses) + ", " + context.message.author.mention)
+@client.event
+async def on_message(message):
+    await client.process_commands(message)
+    if message.author == client.user:
+        return
+
+    if message.author.id == '124969272698077184': #HY
+        author = message.author
+        content = message.content
+        channel = message.channel
+        await client.send_message(channel,"SUP HY!")
+
+    if message.author.id == '148338773887811587': #Chi
+        author = message.author
+        content = message.content
+        channel = message.channel
+        await client.add_reaction(message,':yikes:589332576909525012')
+
+    if message.author.id == '136297703029080065': #CB
+        author = message.author
+        content = message.content
+        channel = message.channel
+        await client.send_message(channel,"Phanpy stuff I guess")
+
+    if message.author.id == '102636726811389952': #Anthony
+        author = message.author
+        content = message.content
+        channel = message.channel
+        await client.add_reaction(message,'a:hc:659191821506838528')
+
+    if message.author.id == '137291885130678272': #Mike
+        author = message.author
+        content = message.content
+        channel = message.channel
+        await client.send_message(channel,"1 gift for 2 days")
+
+    if message.author.id == '122867560210235392': #Jen
+        author = message.author
+        content = message.content
+        channel = message.channel
+        await client.add_reaction(message,'🎂')
+        await client.send_message(channel,"Happy Birthday!!!")
+
+    if message.author.id == '96746583445475328': #Mark
+        author = message.author
+        content = message.content
+        channel = message.channel
+        await client.send_message(channel,"I support breast cancer")
+    
+    if message.author.id == '97590532380827648': #Law
+        author = message.author
+        content = message.content
+        channel = message.channel
+        #emoji = '\:slight_smile: '
+        #await client.send_message(channel,"I support breast cancer")
+        #await client.add_reaction(message,':yikes:589332576909525012')
+        #await client.add_reaction(message,'a:hc:659191821506838528')
+        return
 
 @client.command(name='playwhat',
                 description="TBD",
@@ -46,65 +94,12 @@ async def whatGame(context):
         'Brawlhalla',
         'Mabinogi',
         'Pokemon',
-        'Dont starve together',
+        'Dont Starve Together',
         'Monaco',
         'Left 4 Dead',
-        'Black Desert Online',
     ]
     await client.say(random.choice(possible_responses))
-#dasdsadsa
-@client.command(name='steamTest',
-                description="TBD",
-                brief="TBD",
-                pass_context=True)
-async def steamAPI(context, arg):
-    id = arg
-    url = 'http://api.steampowered.com/ISteamNews/GetNewsForApp/v0002/?appid=%s&count=3&maxlength=300&format=json' % id
-    response = requests.get(url)
-    await client.say(id)
-    await client.say(url)
-    await client.say(response.json())
-
-@client.command(name='boredom',
-                description="TBD",
-                brief="TBD",
-                pass_context=True)
-async def boredom(context, arg):
-    number = int (arg)
-    number = number*number
-    await client.say(number)
 
 
-#@client.command()
-#async def square(number):
-#    squared_value = int(number) * int(number)
-#    await client.say(str(number) + " squared is " + str(squared_value))
 
-
-#@client.event
-#async def on_ready():
-#    await client.change_presence(game=Game(name="with humans"))
-#    print("Logged in as " + client.user.name)
-
-
-#@client.command()
-#async def bitcoin():
-#    url = 'https://api.coindesk.com/v1/bpi/currentprice/BTC.json'
-#    async with aiohttp.ClientSession() as session:  # Async HTTP request
-#        raw_response = await session.get(url)
-#        response = await raw_response.text()
-#        response = json.loads(response)
-#        await client.say("Bitcoin price is: $" + response['bpi']['USD']['rate'])
-
-
-#async def list_servers():
-#    await client.wait_until_ready()
-#    while not client.is_closed:
-#        print("Current servers:")
-#        for server in client.servers:
-#            print(server.name)
-#        await asyncio.sleep(600)
-
-
-#client.loop.create_task(list_servers())
 client.run(TOKEN)
