3ce6c39156e6dc19ba0aa55f66001d63f317dac9
Knight-Law <xlawdizx@gmail.com>
2020-09-09 17:41:35 -0700

    File Path correction for python

--- main.py
+++ main.py
@@ -53,7 +53,7 @@ async def load(context, extension):
 async def unload(context, extension):
     client.unload_extension(f'cogs.{extension}')
 
-for filename in os.listdir("Cogs/"):
+for filename in os.listdir("cogs/"):
     if filename.endswith('.py'):
         client.load_extension(f'cogs.{filename[:-3]}')
 
